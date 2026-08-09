54f1d746a22e572c2eb3cbedb7fc35be3155af1a
Fatimah Shirin <shirinfatimah06@gmail.com>
2026-08-09 14:37:11 +0530

    Wire up Login and Register routes

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import Home from "./pages/Home";
 import OurStory from "./pages/OurStory";
 import HowWeWork from "./pages/HowWeWork";
 import FAQs from "./pages/FAQs";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 import "./App.css";
 
 function App() {
@@ -15,6 +17,8 @@ function App() {
         <Route path="/our-story" element={<OurStory />} />
         <Route path="/how-we-work" element={<HowWeWork />} />
         <Route path="/faqs" element={<FAQs />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </Router>
   );
