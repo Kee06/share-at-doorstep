2e6877b40d1e0c12dda9baa81e5a444a0724dc81
Keerthana <keerthanamanoj6@gmail.com>
2026-08-10 22:43:54 +0530

    fixed error

--- server/index.js
+++ server/index.js
@@ -6,14 +6,14 @@ require('dotenv').config();
 
 
 const pickupRoutes = require('./routes/pickupRoutes');
-const ngoRoutes = require('./routes/ngoRoutes');
+
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../public')));
 app.use('/api/pickups', pickupRoutes);
-app.use('/api/ngos', ngoRoutes);
+
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
